e0b1ea6e2abe9d91acc522a19300f5b120243c5f
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-25 23:22:22 +0000

    add procedure store for correction project

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,14 @@
+-- script that creates a stored procedure AddBonus that adds a new correction for a studen
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
+BEGIN
+    DECLARE id_project INT;
+    SELECT id INTO id_project FROM projects WHERE name = project_name;
+    IF id_project IS NULL
+        INSERT INTO projects (name) VALUES (project_name);
+        SET id_project = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, id_project ,score);
+END;
+$$
+DELIMITER ;
