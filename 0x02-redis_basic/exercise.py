7b121da6b9fb66ab5e13bb2f5ac576598d542600
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-01 21:31:24 +0000

    class cahce using Redis intance

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""blueprint for creation object of Cache"""
+from uuid import uuid4
+from redis import Redis
+from typing import Union
+
+
+class Cache:
+    """implement properties and methods of class Cache"""
+
+    def __init__(self):
+        """ constructor method"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ gen random key and store data in Redis with key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
