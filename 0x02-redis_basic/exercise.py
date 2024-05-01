4e28795e06a55493772fbf9f4785183a15cc2d63
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-01 21:41:58 +0000

    clean exercice

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """blueprint for creation object of Cache"""
-from uuid import uuid4
-from redis import Redis
+import uuid
+import redis
 from typing import Union
 
 
@@ -10,11 +10,11 @@ class Cache:
 
     def __init__(self):
         """ constructor method"""
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ gen random key and store data in Redis with key"""
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+        key_value = str(uuid.uuid4())
+        self._redis.set(key_value, data)
+        return key_value
