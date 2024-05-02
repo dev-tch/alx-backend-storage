d7b9a8710da834214a0a6080560b8bf4416302a1
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-02 00:04:13 +0000

    save inputs and outputs of method using decorator in redis database

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,20 @@ from typing import Callable
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    """ store the history of inputs and outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_list = f"{method.__qualname__}:inputs"
+        output_list = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_list, str(args))
+        method_result = method(self, *args, **kwargs)
+        self._redis.rpush(output_list, str(method_result))
+        return method_result
+    return wrapper
+
+
 def count_calls(method: Callable) -> Callable:
     """count how many times methods of the Cache class are called"""
     @wraps(method)
@@ -25,6 +39,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ gen random key and store data in Redis with key"""
